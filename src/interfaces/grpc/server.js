61e79b52c375ad017d6733878d6f4f10f0ff6ca0
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2025-02-11 23:02:31 +0100

    Add todo gRPC realtime

--- src/interfaces/grpc/server.js
+++ src/interfaces/grpc/server.js
@@ -3,26 +3,29 @@ require('dotenv').config();
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
+const EventEmitter = require("events");
 
-const grpcServerReflection = require('grpc-server-reflection');
+const grpcServerReflection = require("grpc-server-reflection");
 
-const PROTO_PATH = path.join(__dirname, 'proto', 'todo.proto');
+const PROTO_PATH = path.join(__dirname, "proto", "todo.proto");
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
 });
 const todoProto = grpc.loadPackageDefinition(packageDefinition).todo;
 
-const TodoRepository = require('../../repositories/TodoRepository');
-const createTodoUseCaseFactory = require('../../usecases/createTodo');
-const getTodosUseCaseFactory = require('../../usecases/getTodos');
+const TodoRepository = require("../../repositories/TodoRepository");
+const createTodoUseCaseFactory = require("../../usecases/createTodo");
+const getTodosUseCaseFactory = require("../../usecases/getTodos");
 
 const todoRepository = new TodoRepository();
 const createTodo = createTodoUseCaseFactory({ todoRepository });
 const getTodos = getTodosUseCaseFactory({ todoRepository });
+
+const createdTodoEvents = new EventEmitter();
 
 function createTodoHandler(call, callback) {
     try {
@@ -33,6 +36,8 @@ function createTodoHandler(call, callback) {
             title: todo.title,
             completed: todo.completed,
         });
+
+        createdTodoEvents.emit("todo-created", todo);
     } catch (err) {
         callback(err);
     }
@@ -52,27 +57,48 @@ function listTodosHandler(call, callback) {
     }
 }
 
+function subscribeTodosHandler(call) {
+    const listener = (newTodo) => {
+        call.write(newTodo);
+    };
+
+    createdTodoEvents.on("todo-created", listener);
+
+    call.on("cancelled", () => {
+        createdTodoEvents.removeListener("todo-created", listener);
+    });
+}
+
 function main() {
-  const server = new grpc.Server();
-  server.addService(todoProto.TodoService.service, {
-    CreateTodo: createTodoHandler,
-    ListTodos: listTodosHandler
-  });
+    const server = new grpc.Server();
+    server.addService(todoProto.TodoService.service, {
+        CreateTodo: createTodoHandler,
+        ListTodos: listTodosHandler,
+        SubscribeTodos: subscribeTodosHandler,
+    });
 
-  const descriptorPath = path.join(__dirname, 'proto', 'todo_descriptor_set.bin');
-  grpcServerReflection.addReflection(server, descriptorPath);
+    const descriptorPath = path.join(
+        __dirname,
+        "proto",
+        "todo_descriptor_set.bin"
+    );
+    grpcServerReflection.addReflection(server, descriptorPath);
 
-  const host = process.env.SERVER_HOST || "0.0.0.0";
-  const port = process.env.GRPC_PORT || "50051";
-  const bindAddress = `${host}:${port}`;
-  server.bindAsync(bindAddress, grpc.ServerCredentials.createInsecure(), (err, boundPort) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`gRPC server is running on ${bindAddress}`);
-    server.start();
-  });
+    const host = process.env.SERVER_HOST || "0.0.0.0";
+    const port = process.env.GRPC_PORT || "50051";
+    const bindAddress = `${host}:${port}`;
+    server.bindAsync(
+        bindAddress,
+        grpc.ServerCredentials.createInsecure(),
+        (err, boundPort) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(`gRPC server is running on ${bindAddress}`);
+            server.start();
+        }
+    );
 }
 
 main();
