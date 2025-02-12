7bad9a4bc78b040b4a63bf1f5882e1e0d959dd9e
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2025-02-12 04:59:43 +0100

    Add REST

---
+++ src/infrastructure/interfaces/rest/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const TodoRepository = require("@supertodo/database/InMemoryTodoRepository");
+const createTodoUseCase = require("@supertodo/application/usecases/createTodo");
+const listTodosUseCase = require("@supertodo/application/usecases/getTodos");
+
+const todoRepository = new TodoRepository();
+const createTodo = createTodoUseCase({ todoRepository });
+const getTodos = listTodosUseCase({ todoRepository });
+
+const app = express();
+const port = process.env.REST_PORT || 3001;
+
+app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => {
+    try {
+        const todos = getTodos();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post("/todos", (req, res) => {
+    try {
+        const { title, description, completed, author } = req.body;
+        const todo = createTodo({ title, description, completed, author });
+        res.status(201).json(todo);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`REST API server listening on port ${port}`);
+});
